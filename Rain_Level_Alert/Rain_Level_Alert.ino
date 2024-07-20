3a1e9f173f6f8254dd6000b508c0041c8a00f2ef
MANOJ.V_CSE <160569397+Manojvcse@users.noreply.github.com>
2024-07-20 12:57:24 +0530

    Add files via upload

---
+++ Rain_Level_Alert/Rain_Level_Alert.ino
@@ -0,0 +1,35 @@
+#define BLYNK_TEMPLATE_ID "TMPL3g0dhCYKV"
+#define BLYNK_TEMPLATE_NAME "RAIN ALERT"
+#define BLYNK_AUTH_TOKEN "bYXkkpDyGQjhzvBECT56lram1sMFzCcs"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define RAIN_SENSOR_PIN A0
+#define RAIN_SENSOR_THRESHOLD 500 // adjust this value based on your sensor
+
+char ssid[] = "BARANI";
+char pass[] = "NOTHING123";
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{
+  int sensorValue = analogRead(RAIN_SENSOR_PIN);
+  Serial.print("Rain sensor value: ");
+  Serial.println(sensorValue);
+
+  if (sensorValue < RAIN_SENSOR_THRESHOLD) {
+    Blynk.logEvent("rainalert");
+  }
+
+  Blynk.virtualWrite(V0, sensorValue);
+
+  Blynk.run();
+  delay(1000);
+}
